44641eb48f47162d8fbac32de347818d16e385ab
radhikaj <radhikaj@live.com>
2019-06-29 09:22:36 -0700

    Update v0.6 docs

---
+++ apidocs/v0.6/search/all_8.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['module_2eh',['module.h',['../module_8h.html',1,'']]],
+  ['mxcsr',['mxcsr',['../struct__oe__context.html#ae06ca082b0ee76393993625490436af2',1,'_oe_context']]]
+];
---
+++ apidocs/v0.6/search/enumvalues_0.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['_5f_5foe_5fenclave_5ftype_5fmax',['__OE_ENCLAVE_TYPE_MAX',['../types_8h_ade811d495805a2e563e2b5f921b3aad7.html#ade811d495805a2e563e2b5f921b3aad7ad119e05e06a88b18818f8220decf7b56',1,'types.h']]],
+  ['_5foe_5fasymmetric_5fkey_5fformat_5fmax',['_OE_ASYMMETRIC_KEY_FORMAT_MAX',['../types_8h_a95f050e146ca454664cd0283e130397d.html#a95f050e146ca454664cd0283e130397da26dc0d9bfc22e7f2bb5d0c697436e0ab',1,'types.h']]],
+  ['_5foe_5fasymmetric_5fkey_5ftype_5fmax',['_OE_ASYMMETRIC_KEY_TYPE_MAX',['../types_8h_ade70955979f02fc1f0dd8fd34f51f5ff.html#ade70955979f02fc1f0dd8fd34f51f5ffa71f02d022a8bf890ba28643fb1a3d98f',1,'types.h']]],
+  ['_5foe_5fseal_5fpolicy_5fmax',['_OE_SEAL_POLICY_MAX',['../types_8h_ab7977cbd56716f8839315932d5f0f280.html#ab7977cbd56716f8839315932d5f0f280a16a6e9c99c1a0c71919c4b31476ced67',1,'types.h']]]
+];
