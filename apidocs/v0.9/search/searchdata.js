dee9b69f0a22aa2f2a56b92d0ccc1f3a571ebdcd
radhikaj <radhikaj@live.com>
2020-04-23 21:34:19 -0700

    Prepare for v0.9

---
+++ apidocs/v0.9/search/searchdata.js
@@ -0,0 +1,42 @@
+var indexSectionsWithContent =
+{
+  0: "_abcefhimopqrstu",
+  1: "_",
+  2: "ehmprt",
+  3: "o",
+  4: "abcefimprstu",
+  5: "o",
+  6: "_",
+  7: "_oq",
+  8: "o",
+  9: "o"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "files",
+  3: "functions",
+  4: "variables",
+  5: "typedefs",
+  6: "enums",
+  7: "enumvalues",
+  8: "defines",
+  9: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Data Structures",
+  2: "Files",
+  3: "Functions",
+  4: "Variables",
+  5: "Typedefs",
+  6: "Enumerations",
+  7: "Enumerator",
+  8: "Macros",
+  9: "Pages"
+};
+
